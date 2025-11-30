7bc4986c745ee125510b84132d47171bed81ae68
Hafiz Hamza <khawajahamza566@gmail.com>
2025-11-30 11:23:11 +0500

    Update cors (Deployed link)

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  origin: process.env.FRONTEND_URL || "http://localhost:5173","https://babyzhub.com",
+      "https://www.babyzhub.com",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
